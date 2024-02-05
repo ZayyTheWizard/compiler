fce15307fd344023e5a2cc46cd79908976054eb6
ZayyTheWizard <isaiah.washington19@yahoo.com>
2024-02-05 13:58:12 -0500

    Testing and Fixed String token representation

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 use std::env;
 use std::fs;
+use std::process::ExitCode;
 mod tokenizer;
 
-fn main() {
+fn main() -> ExitCode {
     // Taking in source code as arguments
     let args: Vec<String> = env::args().collect();
+    if args.len() == 1 {
+        println!("No source file included");
+        return ExitCode::FAILURE;
+    }
     let file_path = &args[1];
     let contents = fs::read_to_string(file_path).expect("Could Not Read");
 
@@ -16,4 +21,6 @@ fn main() {
     for it in &_tokens_list {
         println!("{:?}", it)
     }
+
+    return ExitCode::SUCCESS;
 }
--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -17,11 +17,50 @@ enum TokenType {
     Minus,
     Multiply,
     Divide,
+    Int,
 }
 
 #[derive(Debug)]
+#[allow(dead_code)]
 pub struct Tokens {
     token_type: TokenType,
+}
+
+fn token_match(tokens_list: &mut Vec<Tokens>, cache: &mut String) {
+    match cache.as_str() {
+        "func" => {tokens_list.push(Tokens { token_type: TokenType::Func }); cache.clear();},
+        "int" => {tokens_list.push(Tokens { token_type: TokenType::Int }); cache.clear();},
+        "=" => {tokens_list.push(Tokens { token_type: TokenType::Eq }); cache.clear();},
+        "," => {tokens_list.push(Tokens { token_type: TokenType::Comma }); cache.clear();},
+        "{" => {tokens_list.push(Tokens { token_type: TokenType::Ocurly}); cache.clear();},
+        "}" => {tokens_list.push(Tokens { token_type: TokenType::Ccurly}); cache.clear();},
+        "(" => {tokens_list.push(Tokens { token_type: TokenType::Oparan }); cache.clear();},
+        ")" => {tokens_list.push(Tokens { token_type: TokenType::Cparan }); cache.clear();},
+        "void" => {tokens_list.push(Tokens { token_type: TokenType::Void }); cache.clear();},
+        "let" => {tokens_list.push(Tokens { token_type: TokenType::Let }); cache.clear();},
+        ";" => {tokens_list.push(Tokens { token_type: TokenType::Semicolon }); cache.clear();},
+        "shout" => {tokens_list.push(Tokens { token_type: TokenType::Shout }); cache.clear();},
+        "+" => {tokens_list.push(Tokens { token_type: TokenType::Plus }); cache.clear();},
+        "-" => {tokens_list.push(Tokens { token_type: TokenType::Minus }); cache.clear();},
+        "*" => {tokens_list.push(Tokens { token_type: TokenType::Multiply }); cache.clear();},
+        "/" => {tokens_list.push(Tokens { token_type: TokenType::Divide }); cache.clear();},
+        _ => {}
+    }
+}
+
+fn to_num(tokens_list: &mut Vec<Tokens>, cache: &mut String) {
+    match cache.parse::<i32>() {
+        Ok(val) => {
+            tokens_list.push(Tokens {
+                token_type: TokenType::Intlit(val),
+            });
+        }
+        _ => {
+            tokens_list.push(Tokens {
+                token_type: TokenType::Identifier(cache.clone()),
+            });
+        }
+    }
 }
 
 pub fn tokenize (s: &String) -> Vec<Tokens> {
@@ -30,61 +69,36 @@ pub fn tokenize (s: &String) -> Vec<Tokens> {
 
     for c in s.chars() {
         if (c == ('(') || c == (')') || c == (';') || c == (','))  && !cache.is_empty() {
-            tokens_list.push(Tokens {
-                token_type: TokenType::Identifier(cache.clone()),
-            });
+            to_num(&mut tokens_list, &mut cache);
             cache.clear();
             cache.push(c);
-            match cache.as_str() {
-                "(" => {tokens_list.push(Tokens { token_type: TokenType::Oparan }); cache.clear();},
-                ")" => {tokens_list.push(Tokens { token_type: TokenType::Cparan }); cache.clear();},
-                ";" => {tokens_list.push(Tokens { token_type: TokenType::Semicolon }); cache.clear();},
-                "," => {tokens_list.push(Tokens { token_type: TokenType::Comma }); cache.clear();},
-                _ => {}
-            }
+            token_match(&mut tokens_list, &mut cache);
             cache.clear();
             continue;
         }
+        if (cache.chars().nth(0) == Some('"') && c != '"') || (cache.chars().nth(0) == Some('"') && c == '"') {
+            if c == '"' {
+                cache.push(c);
+                tokens_list.push(Tokens {token_type: TokenType::Identifier(cache.clone()),});
+                cache.clear();
+                continue;
+            }
+            else {
+                cache.push(c);
+                continue;
+            }
+        }
         if c.is_ascii_whitespace(){
             if !cache.is_empty() {
-                match cache.parse::<i32>() {
-                    Ok(val) => {
-                        tokens_list.push(Tokens {
-                            token_type: TokenType::Intlit(val),
-                        });
-                    }
-                    _ => {
-                        tokens_list.push(Tokens {
-                            token_type: TokenType::Identifier(cache.clone()),
-                        });
-                    }
-                }
+                to_num(&mut tokens_list, &mut cache);
                 cache.clear();
             }
             continue;
         }
         cache.push(c);
-
-        match cache.as_str() {
-            "func" => {tokens_list.push(Tokens { token_type: TokenType::Func }); cache.clear();},
-            "=" => {tokens_list.push(Tokens { token_type: TokenType::Eq }); cache.clear();},
-            "," => {tokens_list.push(Tokens { token_type: TokenType::Comma }); cache.clear();},
-            "{" => {tokens_list.push(Tokens { token_type: TokenType::Ocurly}); cache.clear();},
-            "}" => {tokens_list.push(Tokens { token_type: TokenType::Ccurly}); cache.clear();},
-            "(" => {tokens_list.push(Tokens { token_type: TokenType::Oparan }); cache.clear();},
-            ")" => {tokens_list.push(Tokens { token_type: TokenType::Cparan }); cache.clear();},
-            "void" => {tokens_list.push(Tokens { token_type: TokenType::Void }); cache.clear();},
-            "let" => {tokens_list.push(Tokens { token_type: TokenType::Let }); cache.clear();},
-            ";" => {tokens_list.push(Tokens { token_type: TokenType::Semicolon }); cache.clear();},
-            "shout" => {tokens_list.push(Tokens { token_type: TokenType::Shout }); cache.clear();},
-            "+" => {tokens_list.push(Tokens { token_type: TokenType::Plus }); cache.clear();},
-            "-" => {tokens_list.push(Tokens { token_type: TokenType::Minus }); cache.clear();},
-            "*" => {tokens_list.push(Tokens { token_type: TokenType::Multiply }); cache.clear();},
-            "/" => {tokens_list.push(Tokens { token_type: TokenType::Divide }); cache.clear();},
-            _ => {}
-        }
+        
+        token_match(&mut tokens_list, &mut cache);
     }
-
     if !cache.is_empty() {
         tokens_list.push(Tokens {
             token_type: TokenType::Identifier(cache),
